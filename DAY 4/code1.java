908b6dbbd79aa24da6a27eda5229bb4abed8dffb
ganeshgodase4594 <godaseganesh4594@gmail.com>
2023-10-24 11:23:58 +0530

    DAY 4

---
+++ DAY 4/code1.java
@@ -0,0 +1,50 @@
+
+/*
+
+   Program 1 : WAP to print the following pattern
+               Take input from user
+
+               A  B  C  D
+               #  #  #  #
+               A  B  C  D
+               #  #  #  # 
+*/
+
+import java.util.*;
+
+class Solution {
+	
+	static void printPattern(int row,int col) {
+
+		int ch = 65;
+
+		for(int i=1; i<=row; i++) {
+
+			for(int j=1; j<=col; j++) {
+
+				if(i%2 == 1) {
+
+					System.out.print((char)(ch)+"  ");
+					ch++;
+				}else {
+
+					System.out.print("#  ");
+					ch--;
+				}
+			}
+			System.out.println();
+		}
+	}	
+        public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter number of row : ");
+		int row = sc.nextInt();
+
+        System.out.println("Enter number of col: ");
+		int col = sc.nextInt();
+
+		printPattern(row,col);
+	}
+}
